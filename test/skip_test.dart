a4185d7859bf1b6f68d072e812cf92bd9130097c
epul2207 <epuls073@gmail.com>
2023-09-17 23:59:46 +0700

    Belajar skip function

---
+++ test/skip_test.dart
@@ -0,0 +1,22 @@
+//@Skip('Ini di skip dulu yah')
+
+import 'package:test/test.dart';
+
+String sayHello(String nama) {
+  return 'Hai $nama';
+}
+
+void main() {
+  tearDownAll(() {
+    print('Ini contoh teardown function');
+  });
+  group('Tear Downl All', () {
+    test('pertama', () {
+      expect(sayHello('Otong'), 'Hai Otong');
+    });
+
+    test('kedua', () {
+      expect(sayHello('Ucup'), 'Hai Ucup');
+    }, skip: 'Di skip dulu yah');
+  });
+}
