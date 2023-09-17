52d1ce259df711c3b77a72ce711296d0f2bab4c7
epul2207 <epuls073@gmail.com>
2023-09-17 14:06:49 +0700

    Belajar Matcher

---
+++ test/matcher_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  test("Test Sum", () {
+    expect(sum(1, 1), lessThan(3));
+    expect(sum(3, 3), lessThan(7));
+  });
+}
